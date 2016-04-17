a92b2acdf916d0f4ab7470c69d0f6cdf3aa95cae
Avikalp7 <avikalp22@gmail.com>
2016-04-18 03:02:19 +0530

    Add department rank feature

--- cg_accumulator.py
+++ cg_accumulator.py
@@ -13,11 +13,12 @@ def is_number(s):
     except ValueError:
         return False
 
-def cg_accumulate(year, dep, degree_choice, sg_cg_choice):
+def cg_accumulate(year, dep, degree_choice, sg_cg_choice, user_cg):
 	
 	# List of departments with Integrated M.Sc. (5 year courses)
 	msc_dep_list = ["GG", "EX", "MA", "CY", "HS", "PH"]
 	msc_dep = False
+	dep_rank = 1
 	
 	print ""
 	fname = "Output.txt"
@@ -75,11 +76,15 @@ def cg_accumulate(year, dep, degree_choice, sg_cg_choice):
 				name = line[24:idx]
 				name_flag = True
 
-			if sg_cg_choice == "1":
+			if sg_cg_choice == "1" or sg_cg_choice == "4":
 				if line.find("CGPA") != -1:
 					if line[4] != "<" and is_number(line[31:35]):
-						print "Roll Num : " + str(rollno) + "	CG : " + str(line[31:35]) + "	Name : " + str(name)
-						cg_total += float(line[31:35])
+						if sg_cg_choice == "4":
+							if user_cg < float(line[31:35]):
+								dep_rank += 1
+						else:
+							print "Roll Num : " + str(rollno) + "	CG : " + str(line[31:35]) + "	Name : " + str(name)
+							cg_total += float(line[31:35])
 						break
 			elif sg_cg_choice == "2":
 				if line.find("SGPA") != -1 and is_number(line[25:29]):
@@ -114,7 +119,40 @@ def cg_accumulate(year, dep, degree_choice, sg_cg_choice):
 	elif sg_cg_choice == "2":
 		print "Total SG : " + str(sg_total)
 		print "Average SG : " + str(sg_total / student_count)
+	elif sg_cg_choice == "4":
+		print "Your Department Rank is : " + str(dep_rank)
+		if dep_rank < student_count/2:
+			print "Good going. You are in the top half of your department"
+		else:
+			print "A bit more hard work can see you in the top half of your department"
 	print "__________________________________"
+
+
+def find_cg(roll_num):
+	url_to_scrape = 'https://erp.iitkgp.ernet.in/StudentPerformance/view_performance.jsp?rollno=' + str(roll_num)
+	fname = "Output.txt"
+	try:
+		r = requests.get(url_to_scrape) 
+
+		soup = BeautifulSoup(r.text, "html.parser") 
+
+		with open(fname, "w") as text_file:
+			text_file.write("{}".format(soup))
+
+		with open(fname) as f:
+			content = f.readlines()
+
+		if len(content) < 40:
+			print "Invalid Roll Number!"
+		else:
+			for line in content:
+				if line.find("CGPA") != -1:
+					if line[4] != "<" and is_number(line[31:35]):
+						return float(line[31:35])
+	except Exception:
+		print "ConnectionError on you roll number. Please check your connection and try again!"
+		key = raw_input("The program will now exit. Press enter")
+		exit(0)
 
 
 
@@ -122,6 +160,7 @@ print "*** Welcome to CG Accumulator ***"
 
 departments = ["AE", "AG", "AR", "BT", "CE", "CH", "CS", "CY", "EC", "EE", "EX", "GG", "HS", "IE", "IM", "MA", "ME", "MF", "MI", "MT", "NA", "PH", "QD"]
 years = ["12","13","14","15"]
+user_cg = 0.00
 
 while True:
 	print ""
@@ -136,15 +175,23 @@ while True:
 		print "P.S. Department name should be capitalised. Eg. \"CS\" and not \"cs\""
 		dep = raw_input("Enter Valid Department again : ")
 	print ""
-	degree_choice = raw_input("Enter choice : '1' for 4 years only, '2' for 5 years only, '3' for both :  ")
-	while degree_choice not in ["1", "2", "3"]:
-		print "Please enter a valid choice!"
-		degree_choice = raw_input("Enter valid choice again : ")
-	print ""
-	sg_cg_choice = raw_input("Do you want CG list (enter '1') or most recent SG list (enter '2') or entire SG history (enter '3')? :  ")
-	while sg_cg_choice not in ["1", "2", "3"]:
+	sg_cg_choice = raw_input("Do you want CG list (enter '1') \n or Most recent SG list (enter '2') \n or Entire SG history (enter '3') \n or Know your D.R. (enter '4')? :  ")
+	while sg_cg_choice not in ["1", "2", "3", "4"]:
 		print "Please enter a valid choice!"
 		sg_cg_choice = raw_input("Enter valid choice again : ")
+
+	if sg_cg_choice == "4":
+		roll_num = raw_input("Enter last 5 digits of your roLl number :  ")
+		while len(roll_num) != 5:
+			print "Please enter valid last 5 digits"
+		user_cg = find_cg(year + dep + roll_num)
+		degree_choice = "3"
+	else:
+		degree_choice = raw_input("Enter choice : '1' for 4 years only, '2' for 5 years only, '3' for both :  ")
+		while degree_choice not in ["1", "2", "3"]:
+			print "Please enter a valid choice!"
+			degree_choice = raw_input("Enter valid choice again : ")
+		print ""
 	break
 
 
@@ -152,7 +199,7 @@ print ""
 print "Please wait while results are being accumulated, this may take a few minutes...."
 print "Meanwhile, minimize this screen and think about what you are doing with your life."
 print ""
-var = cg_accumulate(year, dep, degree_choice,sg_cg_choice)
+var = cg_accumulate(year, dep, degree_choice,sg_cg_choice, user_cg)
 print ""
 key = raw_input("Press Enter to exit")
 
